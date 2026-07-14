f1decb96d69368a8c1163402adb8748992906daa
NKTKLN <nktkln@nktkln.com>
2026-07-14 08:44:58 +0300

    refactor(engine): drop TeacherForcedTrainer

--- src/dl_roadmap/engine/__init__.py
+++ src/dl_roadmap/engine/__init__.py
@@ -2,12 +2,10 @@
 
 from dl_roadmap.engine.class_predictor import ClassPredictor, PredictorConfig
 from dl_roadmap.engine.trainer import Trainer, TrainerConfig
-from dl_roadmap.engine.trainers import TeacherForcedTrainer
 
 __all__ = [
     "ClassPredictor",
     "PredictorConfig",
-    "TeacherForcedTrainer",
     "Trainer",
     "TrainerConfig",
 ]
--- src/dl_roadmap/engine/trainers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Custom Trainer subclasses for specialized training loops."""
-
-from dl_roadmap.engine.trainers.teacher_forced import TeacherForcedTrainer
-
-__all__ = ["TeacherForcedTrainer"]
